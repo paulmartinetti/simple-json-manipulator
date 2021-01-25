19c5b030054fde5f6e12f0afec2032e9682e0163
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 10:33:28 -0500

    works with multi-json

--- script.js
+++ script.js
@@ -5,20 +5,97 @@
 // json 
 let data;
 // params dans chaque json
+// keyA = ["startDelay", "speed"..etc];
 let keyA = [];
+let keys = false;
 // len = 9, du 0 au 8 chaque boucle de var "i"
-let len;
+let len = 0;
 
-// les noms des clients, chacun son json à lui
-let clientA = ["D1_Eco_50", "D2_Janisol", "Janisol_2"];
-let clientAlen = clientA.length;
-
-
+// les noms des projets, chacun son json à lui
+let projetA = ["D1_Eco_50", "D2_Janisol", "Janisol_2"];
+let projetAlen = projetA.length;
+let projInd = 0;
 
 // du gui, on va remplir ça une fois avec step, min, max
 let sliderA = [];
-// indice actuel, 0 corréspond à D1_Eco 50, 1 corréspond à D2_Janisol
-let indAct = 0;
+
+/**
+ * 
+ * Pulldown = projet selector
+ */
+function setupPulldown() {
+
+  // il n'y a qu'un seul selector
+  let dropdown = document.getElementById('designation-dropdown');
+  // initialiser
+  dropdown.length = 0;
+  dropdown.selectedIndex = 0;
+  // utilise projetA
+  let option;
+  for (let i = 0; i < projetAlen; i++) {
+    option = document.createElement('option');
+    option.text = projetA[i];
+    option.value = i;
+    dropdown.add(option);
+  }
+
+  // initialiser selector
+  selectOnChange(0);
+}
+
+// 
+function selectOnChange(curSelVal) {
+  projInd = curSelVal;
+  // rechercher le json de nouveau à chaque appuis du selector
+  let url = projetA[projInd] + ".json"
+  request.open('GET', url, true);
+  request.send();
+}
+
+
+/**
+ * rechercher un json
+ */
+const request = new XMLHttpRequest();
+request.onload = function () {
+  if (request.status === 200) {
+    // c'est bon
+    data = JSON.parse(request.responseText);
+    getKeys();
+  } else {
+    // Reached the server, but it returned an error
+  }
+  // initial
+  guiUpdate();
+}
+request.onerror = function () {
+  console.error('An error occurred fetching the JSON from ' + url);
+};
+
+function getKeys(){
+
+  // une fois
+  if(keys) return;
+
+  keyA = Object.keys(data);
+  len = keyA.length;
+  // remplir global var pour gérer le gui
+  for (let i = 0; i < len; i++) {
+    // prendre des données du gui
+    let s = document.getElementById(keyA[i]);
+    let obj = {
+      step: s.step,
+      min: s.min,
+      max: s.max
+    };
+    // step, min, max de chaque slider
+    sliderA.push(obj);
+  }
+  keys = true;
+}
+
+// c'est parti - appelle une foi
+setupPulldown();
 
 /**
  * ALWAYS update gui from data
@@ -29,19 +106,19 @@ function guiUpdate() {
 
   // m-à-j gui des données
   for (let i = 0; i < len; i++) {
-    // nom, comme "balayage"
-    let nom = dataNomA[i];
+    // param comme "balayage"
+    let param = keyA[i];
     // sa valeur il faut forcer Number() sinon il est traité comme texte (String)
-    let val = Number(data[indAct][nom]);
+    let val = Number(data[param]);
 
     // il faut chercher de nouveau chaque slider
-    let s = document.getElementById(nom);
+    let s = document.getElementById(param);
     // et sa valeur montrée
-    let sv = document.getElementById(nom + "Val");
+    let sv = document.getElementById(param + "Val");
 
     // les boutons plus et moins, puisque on change leurs états en fonction de min max
-    let bp = document.getElementById(nom + "Plus");
-    let bm = document.getElementById(nom + "Moins");
+    let bp = document.getElementById(param + "Plus");
+    let bm = document.getElementById(param + "Moins");
 
     // update gui
     sv.innerHTML = s.value = val;
@@ -53,100 +130,17 @@ function guiUpdate() {
 }
 
 /**
- * populate select option w json
- * https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
- */
-
-// il n'y a qu'un seul selector
-let dropdown = document.getElementById('designation-dropdown');
-// initialiser
-dropdown.length = 0;
-dropdown.selectedIndex = 0;
-
-const url = 'reglages.json';
-
-// 
-const request = new XMLHttpRequest();
-request.open('GET', url, true);
-
-request.onload = function () {
-  if (request.status === 200) {
-    // c'est bon
-    data = JSON.parse(request.responseText);
-    // remplir selector
-    setupPulldown();
-  } else {
-    // Reached the server, but it returned an error
-  }
-  // initial
-  guiUpdate();
-}
-request.onerror = function () {
-  console.error('An error occurred fetching the JSON from ' + url);
-};
-
-// c'est parti -- premier appel à une fonction
-request.send();
-
-/**
- * selector onChange()
- * https://www.w3schools.com/jsref/event_onchange.asp
- * 
- * Pulldown = selector
- */
-function setupPulldown() {
-
-  let option;
-  for (let i = 0; i < data.length; i++) {
-    option = document.createElement('option');
-    option.text = data[i].name;
-    option.value = data[i].id;
-    dropdown.add(option);
-  }
-
-  // remplir global var pour gérer le gui
-  for (let i = 0; i < len; i++) {
-    let s = document.getElementById(dataNomA[i]);
-    let obj = {
-      step: s.step,
-      min: s.min,
-      max: s.max
-    };
-    // step, min, max de chaque slider
-    sliderA.push(obj);
-  }
-
-  // une fois seulement (on appel le json à chaque appuis sur selector)
-  first = false;
-}
-// 
-function selectOnChange(curSelVal) {
-  // capter l'indAct = l'indice actuel du json (un groupe de valeurs)
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].id == curSelVal) {
-      // trouvé !
-      indAct = i;
-      break;
-    }
-  }
-  // rechercher le json de nouveau à chaque appuis du selector
-  request.open('GET', url, true);
-  request.send();
-}
-
-
-/**
 * slider onChange() -- si tu appuis sur un slider, on m-à-j les données
 * https://stackoverflow.com/questions/13896685/html5-slider-with-onchange-function
 */
 function getSlider(id, newVal) {
   for (let i = 0; i < len; i++) {
     // recherche dans tous les params (balayage, speed, etc)
-    let nom = dataNomA[i];
+    let param = keyA[i];
     // param touché
-    if (id == nom) {
+    if (id == param) {
       // nouvelle valeur du slider, ça vient du gui
-      data[indAct][nom] = newVal;
+      data[param] = newVal;
       break;
     }
   }
@@ -162,14 +156,14 @@ function getSlider(id, newVal) {
 function onPlus(monId) {
   for (let i = 0; i < len; i++) {
     // scan params
-    let nom = dataNomA[i];
+    let param = keyA[i];
     // identifier le bouton touché
-    if (monId.includes(nom)) {
-      let n = Number(data[indAct][nom]);
+    if (monId.includes(param)) {
+      let n = Number(data[param]);
       // ajoute step à la valeur actuelle
       n += Number(sliderA[i].step);
       // m-à-j les données
-      data[indAct][nom] = n.toFixed(2);
+      data[param] = n.toFixed(2);
       break;
     }
   }
@@ -179,12 +173,12 @@ function onPlus(monId) {
 function onMoins(monId) {
   for (let i = 0; i < len; i++) {
     // scan params
-    let nom = dataNomA[i];
+    let param = keyA[i];
     // update param of touched slider
-    if (monId.includes(nom)) {
-      let n = Number(data[indAct][nom]);
+    if (monId.includes(param)) {
+      let n = Number(data[param]);
       n -= Number(sliderA[i].step);
-      data[indAct][nom] = n.toFixed(2);
+      data[param] = n.toFixed(2);
       break;
     }
   }
